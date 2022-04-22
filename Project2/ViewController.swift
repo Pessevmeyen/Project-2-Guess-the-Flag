dbd1d41f4b9666b92081573ee8392f6fbe0e1267
Furkan <gitaristler.toplulugu@gmail.com>
2022-04-23 01:40:18 +0300

    .

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]() // The first line is something you saw in project 1: it creates a new property called countries that will hold a new array of strings.
     var score = 0 // The second one creates a new property called score that is set to 0
     var correctAnswer = 0 // Doğru cevap için bir değişken oluşturuyoruz.
+    var counter = 0 // bir tane sayaç oluşturduk, bununla gelen soruları sayıcaz
     
     
     
@@ -23,18 +24,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-       // countries.append("estonia")
-       // countries.append("france")
-       // countries.append("germany")
-       // countries.append("ireland")
-       // countries.append("italy")
-       // countries.append("monaco")
-       // countries.append("nigeria")
-       // countries.append("poland")
-       // countries.append("russia")
-       // countries.append("spain")
-       // countries.append("uk")
-       // countries.append("us")
+        // countries.append("estonia")
+        // countries.append("france")
+        // countries.append("germany")
+        // countries.append("ireland")
+        // countries.append("italy")
+        // countries.append("monaco")
+        // countries.append("nigeria")
+        // countries.append("poland")
+        // countries.append("russia")
+        // countries.append("spain")
+        // countries.append("uk")
+        // countries.append("us")
         
         //Bu şekilde de yapabiliriz ama array yöntemiyle tek satırda yazabiliriz
         
@@ -51,7 +52,7 @@ class ViewController: UIViewController {
         askQuestion()
         
     }
-
+    
     
     func askQuestion(action: UIAlertAction! = nil) {
         // askQuestion(action: UIAlertAction! ise yukardaki askQuestion(action: nil) olacak
@@ -70,38 +71,60 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage((UIImage(named: countries[2])), for: .normal)
         
-        title = countries[correctAnswer].uppercased() // başlığı büyütmek için bunu yazıyoruz
-        
+        title = "Score: \(score), " + "Tap on: " + countries[correctAnswer].uppercased() + "'s Flag" // Kullanıcının score'unu ve seçmesi gereken bayrağı söylüyoruz.
         
         
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) { // 1. Check whether the answer was correct.
-                                                      // 2. Adjust the player's score up or down.
-                                                      // 3. Show a message telling them what their new score is
+        // 2. Adjust the player's score up or down.
+        // 3. Show a message telling them what their new score is
         
         var title: String
         
         if sender.tag == correctAnswer { // But how do we know whether the correct button was tapped? Right now, all the buttons look the same, but behind the scenes all views have a special identifying number that we can set, called its Tag. This can be any number you want, so we're going to give our buttons the numbers 0, 1 and 2. This isn't a coincidence: our code is already set to put flags 0, 1 and 2 into those buttons, so if we give them the same tags we know exactly what flag was tapped.
             title = "Correct"
             score += 1
+            counter += 1 // Burada ve aşağıda yazdığımız gibi, doğru da olsa yanlış da olsa sayaç işlicek
         } else {
-            title = "Wrong"
+            title = "Wrong! That's the flag of \(countries[sender.tag].uppercased())" // Eğer kullanıcı yanlış bayrağı seçerse, seçtiği bayrağın ismini gösterecek.
             score -= 1
+            counter += 1
         }
         
-        let ac = UIAlertController(title: title, message: "your score is \(score)", preferredStyle: .alert) // UIAlertController() is used to show an alert with options to the user. First new thing there is the .alert parameter being used for preferredStyle. preferredStyle hangi şekille bildirim göstereceğini gösteriyor. Burada Closure yaptık
-        
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) // The second line uses the UIAlertAction data type to add a button to the alert that says "Continue", and gives it the style “default". önceden hatırlarsak 3 çeşit style vardı: .default, .cancel, .destructive. The "handler" parameter is looking for a closure, which is some code that it can execute when the button is tapped. We want the game to continue when the button is tapped, so we pass in askQuestion so that iOS will call our askQuestion() method.
-        
-        present(ac, animated: true) // The final line calls present(), which takes two parameters: a view controller to present and whether to animate the presentation.
-        
+        if counter == 6 { // ve sayaç 10'a ulaşınca ilk mesaj gösterilecek.
+            let acFinish = UIAlertController(title: title, message: "GAME OVER! Your Score is \(score)", preferredStyle: .alert) // UIAlertController() is used to show an alert with options to the user. First new thing there is the .alert parameter being used for preferredStyle. preferredStyle hangi şekille bildirim göstereceğini gösteriyor. Burada Closure yaptık
+            acFinish.addAction(UIAlertAction(title: "START AGAIN", style: .default, handler: askQuestion)) // The second line uses the UIAlertAction data type to add a button to the alert that says "Continue", and gives it the style “default". önceden hatırlarsak 3 çeşit style vardı: .default, .cancel, .destructive. The "handler" parameter is looking for a closure, which is some code that it can execute when the button is tapped. We want the game to continue when the button is tapped, so we pass in askQuestion so that iOS will call our askQuestion() method.
+            present(acFinish, animated: true)
+            score = 0
+            counter = 0
+            
+        } else {
+            
+            let ac = UIAlertController(title: title, message: "your score is \(score)", preferredStyle: .alert) // UIAlertController() is used to show an alert with options to the user. First new thing there is the .alert parameter being used for preferredStyle. preferredStyle hangi şekille bildirim göstereceğini gösteriyor. Burada Closure yaptık
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) // The second line uses the UIAlertAction data type to add a button to the alert that says "Continue", and gives it the style “default". önceden hatırlarsak 3 çeşit style vardı: .default, .cancel, .destructive. The "handler" parameter is looking for a closure, which is some code that it can execute when the button is tapped. We want the game to continue when the button is tapped, so we pass in askQuestion so that iOS will call our askQuestion() method.
+            present(ac, animated: true) // The final line calls present(), which takes two parameters: a view controller to present and whether to animate the presentation.
+        }
     }
     
+    @IBAction func scoreButtonTapped(_ sender: UIBarButtonItem) {
+        
+        let scoreAlert = UIAlertController(title: "Your score is \(score) ", message: nil, preferredStyle: .alert)
+        
+        scoreAlert.addAction(UIAlertAction(title: "Continue Playing", style: .cancel))
+        present(scoreAlert, animated: true)
+    
+    }
+        
+        
     
     
     
-    
-
 }
+    
+    //        let ac = UIAlertController(title: title, message: "your score is \(score)", preferredStyle: .alert) // UIAlertController() is used to show an alert with options to the user. First new thing there is the .alert parameter being used for preferredStyle. preferredStyle hangi şekille bildirim göstereceğini gösteriyor. Burada Closure yaptık
+    //        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) // The second line uses the UIAlertAction data type to add a button to the alert that says "Continue", and gives it the style “default". önceden hatırlarsak 3 çeşit style vardı: .default, .cancel, .destructive. The "handler" parameter is looking for a closure, which is some code that it can execute when the button is tapped. We want the game to continue when the button is tapped, so we pass in askQuestion so that iOS will call our askQuestion() method.
+    //        present(ac, animated: true) // The final line calls present(), which takes two parameters: a view controller to present and whether to animate the presentation.
+
+
 
